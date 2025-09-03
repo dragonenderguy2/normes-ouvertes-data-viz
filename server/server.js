6304fe3cad190b5ed4892fef62c2ccc1a70c0567
dragonenderguy2 <shaihalapre@hotmail.com>
2025-09-03 12:08:56 -0500

    feat: initial commit for server/server.js

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get('/', (req, res) => {
+  res.send('API fonctionne !');
+});
+
+app.listen(PORT, () => {
+  console.log(`Le serveur est démarré sur le port ${PORT}`);
+});
