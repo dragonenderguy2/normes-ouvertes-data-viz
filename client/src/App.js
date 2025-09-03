3bb08c533527e28a83f0717d6213abc105095552
dragonenderguy2 <shaihalapre@hotmail.com>
2025-09-03 12:08:50 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Normes Ouvertes Data Viz</h1>
+      </header>
+      <main>
+        <p>Bienvenue sur la plateforme de visualisation des normes ouvertes.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
